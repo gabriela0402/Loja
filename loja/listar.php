ee7cc08c7ded235f15036865314edcbb8e5c1eaa
gabriela0402 <gabiromano2018@gmail.com>
2025-08-29 16:53:58 -0300

    atualizar

--- loja/listar.php
+++ loja/listar.php
@@ -31,8 +31,8 @@
                 echo"
                     <td>
                         <div class='btn-group' role='group' aria-label='Basic mixed styles example'>
-                            <a href='#' type='button' class='btn btn-danger'>Cadastrar</a>
-                            <a href='#' type='button' class='btn btn-warning'>Listar</a>
+                            <a href='form_atualizar.php?id=".$produto['id']."' type='button' class='btn btn-danger'>Atualizar</a>
+                            <a href='#' type='button' class='btn btn-warning'>Apagar</a>
                         </div>
                     </td>
                 ";
